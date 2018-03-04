187d03b9d10e9a012c81920fda2c1c6bcc6c3c65
Prashanth <prashanths1992@gmail.com>
2018-03-04 17:58:03 -0500

    Update attack.js

--- attack.js
+++ attack.js
@@ -1,1 +1,5 @@
-<script> window.location.href = 'https://raw.githubusercontent.com/prashanths1992/System-Security/master/yhhbh.png' </script>
+<script>
+
+  window.location.href = 'https://raw.githubusercontent.com/prashanths1992/System-Security/master/yhhbh.png';
+
+</script>
